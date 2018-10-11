59bbf51a735dfe661550a4f9c91d889b744f164d
Zuhha <zuhhaazhar989@gmail.com>
2018-10-11 12:17:31 +0500

    Create page

---
+++ PhoneBook/Models/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Models
+{
+    public class PersonModel
+    {
+        public string TwitterId { get; set; }
+        public string EmailId { get; set; }
+        public string HomeAddress { get; set; }
+        public string HomeCity { get; set; }
+        public string FacebookAccountId { get; set; }
+        public string LinkedInId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.Date)]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
